2c375624436e29ba161b9331fa8d9c7c480ddb9f
Marek Gargas <250497@wp.pl>
2020-01-04 19:00:04 +0100

    add REST endpoint for retrieving nodes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+import akka.http.scaladsl.server.{HttpApp, Route}
 import de.heikoseeberger.akkahttpjackson.JacksonSupport
 import model.Node
 import org.apache.poi.ss.usermodel.WorkbookFactory
@@ -5,10 +6,18 @@ import parser.NodesParser
 
 import scala.language.postfixOps
 
-object Main extends App with JacksonSupport {
+object Main extends HttpApp with App with JacksonSupport {
+
   private val workbook          = WorkbookFactory.create(getClass.getResourceAsStream("test1.xlsx"))
   private val sheet             = workbook.getSheetAt(0) // Assuming they're in the first sheet here.
   private val nodes: List[Node] = NodesParser.parseSheet(sheet, 3)
-  val nodesJson = JacksonSupport.defaultObjectMapper.writeValueAsString(nodes)
+  private val nodesJson         = JacksonSupport.defaultObjectMapper.writeValueAsString(nodes)
   println(nodesJson)
+
+  override protected def routes: Route = path("nodes") {
+    get {
+      complete(nodes)
+    }
+  }
+  startServer("localhost", 8080)
 }
