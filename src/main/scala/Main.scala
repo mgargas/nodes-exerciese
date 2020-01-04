35cfdb29cbe81a6fe4c5eaafa9524a6ccfd239b7
Marek Gargas <250497@wp.pl>
2020-01-04 18:49:37 +0100

    print nodes in JSON format to stdout

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,14 @@
+import de.heikoseeberger.akkahttpjackson.JacksonSupport
 import model.Node
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import parser.NodesParser
 
 import scala.language.postfixOps
 
-object Main extends App {
+object Main extends App with JacksonSupport {
   private val workbook          = WorkbookFactory.create(getClass.getResourceAsStream("test1.xlsx"))
   private val sheet             = workbook.getSheetAt(0) // Assuming they're in the first sheet here.
   private val nodes: List[Node] = NodesParser.parseSheet(sheet, 3)
-  println(nodes)
+  val nodesJson = JacksonSupport.defaultObjectMapper.writeValueAsString(nodes)
+  println(nodesJson)
 }
